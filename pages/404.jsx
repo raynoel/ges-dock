03c06c5218c862d7b280442e4f1c37616a95f7fc
Raymond <raynoel@gmail.com>
2022-08-11 01:18:03 -0400

    test .env

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,11 +3,19 @@ import Link from 'next/link'
 import Layout from "@/components/Layout.jsx"
 
 export default function NotFoundPage() {
+  console.log(process.env.MYSQL_PASSWORD)
+  console.log(process.env.MYSQL_HOST)
+  console.log(process.env.MYSQL_DATABASE)
+  console.log(process.env.MYSQL_USERNAME)
+  console.log(process.env.MYSQL_PASSWORD)
+  console.log(process.env.MYSQL_PORT)
+  
   return (
     <Layout title='Page Not Found'>
       <div className="not-found">
         <h1><FaExclamationTriangle /> 404</h1>
         <h4>Sorry, there is nothing here</h4>
+        
         <Link href='/'><a>Go Back Home</a></Link>
       </div>
 
