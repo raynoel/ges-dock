6f082653208ab614478afb8928ceb5ce7bc07c05
Raymond <raynoel@gmail.com>
2022-08-11 01:32:52 -0400

    error page centered

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,33 +3,26 @@ import Link from 'next/link'
 import Layout from "@/components/Layout.jsx"
 
 export default function NotFoundPage() {
-  console.log(process.env.MYSQL_PASSWORD)
-  console.log(process.env.MYSQL_HOST)
-  console.log(process.env.MYSQL_DATABASE)
-  console.log(process.env.MYSQL_USERNAME)
-  console.log(process.env.MYSQL_PASSWORD)
-  console.log(process.env.MYSQL_PORT)
   
   return (
     <Layout title='Page Not Found'>
       <div className="not-found">
-        <h1><FaExclamationTriangle /> 404</h1>
+        <h1>404</h1>
+        <FaExclamationTriangle style={{ fontSize: "100px", color: "var(--bs-ges-danger)"}} /> 
         <h4>Sorry, there is nothing here</h4>
-        
+        <p></p>
         <Link href='/'><a>Go Back Home</a></Link>
       </div>
 
-      <style jsx>
-        {`
-          .not-found {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-          }
-        `}
-      </style>
+      <style jsx>{`
+        .not-found {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+      `}</style>
     </Layout>
   )
 }
