122b97d8c66e11aa81d45f553d3e55f0466dc773
Raymond <raynoel@gmail.com>
2022-08-13 10:56:11 -0400

    test 41

--- config/db.js
+++ config/db.js
@@ -9,10 +9,10 @@ export const db = mysql({
     // user     : process.env.MYSQL_USERNAME,
     // password : process.env.MYSQL_PASSWORD,
     // port: parseInt(process.env.MYSQL_PORT),
-    host     : 'us-cdbr-east-06.cleardb.net',
-    database : 'heroku_09c53c78c2ce071',
-    user     : 'bfadef98b3c77f',
-    password : 'c3ffa78e',
+    host     : '107.180.3.248',
+    user     : 'GESM0NTR3AL',
+    password : 'Francois1958',
+    database : 'ges_mh_sheets',
     port: parseInt(3306),
   }
 })
