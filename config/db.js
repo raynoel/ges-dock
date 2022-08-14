73953cf15d3206539d5f9bc3dd27d559ce0b3acb
Raymond <raynoel@gmail.com>
2022-08-13 22:04:32 -0400

    test 43

--- config/db.js
+++ config/db.js
@@ -4,16 +4,16 @@ import mysql from 'serverless-mysql'
 // Cré un obj pour intéragir avec la DB
 export const db = mysql({
   config: {
-    // host     : process.env.MYSQL_HOST,
-    // database : process.env.MYSQL_DATABASE,
-    // user     : process.env.MYSQL_USERNAME,
-    // password : process.env.MYSQL_PASSWORD,
-    // port: parseInt(process.env.MYSQL_PORT),
-    host     : 'us-cdbr-east-06.cleardb.net',
-    user     : 'bfadef98b3c77f',
-    password : 'c3ffa78e',
-    database : 'heroku_09c53c78c2ce071',
-    port: parseInt(3306),
+    host     : process.env.NEXT_PUBLIC_MYSQL_HOST,
+    database : process.env.NEXT_PUBLIC_MYSQL_DATABASE,
+    user     : process.env.NEXT_PUBLIC_MYSQL_USERNAME,
+    password : process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
+    port: parseInt(process.env.NEXT_PUBLIC_MYSQL_PORT),
+    // host     : 'us-cdbr-east-06.cleardb.net',
+    // user     : 'bfadef98b3c77f',
+    // password : 'c3ffa78e',
+    // database : 'heroku_09c53c78c2ce071',
+    // port: parseInt(3306),
   }
 })
 
