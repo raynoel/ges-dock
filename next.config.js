bc572ed41198ab589aab567acebd00b9f8aea9b1
Raymond <raynoel@gmail.com>
2022-08-17 05:53:05 -0400

    fixing CORS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// Fixing Cors header ‘Access-Control-Allow-Origin’ missing
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "http://example.com" },
+        ],
+      },
+    ]
+  },
+}
